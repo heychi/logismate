7de659b98e386d7cccfad98389db300811f5fba4
wonyoung0710 <dnjs4023@bu.ac.kr>
2025-02-12 19:42:55 +0900

    add comments

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import ForwarderRecommendation from "./components/ForwarderRecommendation";
-import SmartContractInfo from "./components/SmartContractInfo";
-import SmartContractDocument from "./components/SmartContractDocument";
-import SmartContractPayment from "./components/SmartContractPayment";
-import SmartContractComplete from "./components/SmartContractComplete";
-import Dashboard from "./components/Dashboard";
-import CargoStatus from "./components/CargoStatus";
-import MonitoringDashboard from "./components/MonitoringDashboard";
-import ContractManagement from "./components/DocumentDashboard";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import ForwarderRecommendation from './components/ForwarderRecommendation';
+import SmartContractInfo from './components/SmartContractInfo';
+import SmartContractDocument from './components/SmartContractDocument';
+import SmartContractPayment from './components/SmartContractPayment';
+import SmartContractComplete from './components/SmartContractComplete';
+import Dashboard from './components/Dashboard';
+import CargoStatus from './components/CargoStatus';
+import MonitoringDashboard from './components/MonitoringDashboard';
+import ContractManagement from './components/DocumentDashboard';
 
+// hangman fighting
 const App = () => {
   return (
     <Router>
@@ -23,8 +24,14 @@ const App = () => {
         <Route path="/contract/complete" element={<SmartContractComplete />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/dashboard/status" element={<CargoStatus />} />
-        <Route path="/dashboard/status/monitoring" element={<MonitoringDashboard />} />
-        <Route path="/dashboard/status/document" element={<ContractManagement />} />
+        <Route
+          path="/dashboard/status/monitoring"
+          element={<MonitoringDashboard />}
+        />
+        <Route
+          path="/dashboard/status/document"
+          element={<ContractManagement />}
+        />
       </Routes>
     </Router>
   );
